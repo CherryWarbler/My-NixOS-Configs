75e085dcfb8023c768e38b81e0347484ac404a0f
Cherry <luisitonavarrohernandez@gmail.com>
2026-01-05 08:44:48 -0600

    Update systemapps.nix

--- core/systemapps.nix
+++ core/systemapps.nix
@@ -2,7 +2,7 @@
 
 {
    environment.systemPackages = with pkgs; [
-     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also instal>
+     vim
      wget
      fastfetch
      gnome-tweaks
